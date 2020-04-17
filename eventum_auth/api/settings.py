5d71f1762fb96aa4697bacc1023f935bdc913b11
diarodriguezmo <33883566+diarodriguezmo@users.noreply.github.com>
2020-04-16 21:39:27 -0500

    Update settings.py

--- eventum_auth/api/settings.py
+++ eventum_auth/api/settings.py
@@ -68,12 +68,18 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'evauthdb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'ev-auth-db-postgres',
+        'PORT': '5432',
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
