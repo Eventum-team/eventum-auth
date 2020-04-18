4d5e253e01c32de25c7e89515835d853162e710e
diarodriguezmo <33883566+diarodriguezmo@users.noreply.github.com>
2020-04-18 01:24:29 -0500

    Update serializers.py

--- eventum_auth/evAuth/serializers.py
+++ eventum_auth/evAuth/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from rest_framework_simplejwt.tokens import UntypedToken
 
 class TokenSerializer(serializers.Serializer):
     """
@@ -12,4 +12,11 @@ class TokenSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id","username", "email", "first_name", "last_name")
+        fields = ("id","username")
+
+class CustomTokenVerifySerializer(serializers.Serializer):
+    token = serializers.CharField()
+    def validate(self, attrs):
+        UntypedToken(attrs['token'])
+
+        return "ok"
