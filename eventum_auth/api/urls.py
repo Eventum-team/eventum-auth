6aea4e2ea03662cb3e0706f39f1341fa0883b4fd
diarodriguezmo <33883566+diarodriguezmo@users.noreply.github.com>
2020-04-18 01:33:01 -0500

    Update urls.py

--- eventum_auth/api/urls.py
+++ eventum_auth/api/urls.py
@@ -6,5 +6,5 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', obtain_jwt_token, name='create-token'),
-    re_path('reg/', include('evAuth.urls'))
+    re_path('auth/', include('evAuth.urls'))
 ]
